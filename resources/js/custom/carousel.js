ac812627239fc12ec4cf2e5d62c38f8d19e3ab0e
Martynas Jankauskas <martynasjank@gmail.com>
2026-05-19 14:14:15 +0300

    Replace carousel nav buttons with mousewheel/trackpad swipe

--- resources/js/custom/carousel.js
+++ resources/js/custom/carousel.js
@@ -1,60 +1,37 @@
 import Swiper from 'swiper';
-import { Navigation, Autoplay } from 'swiper/modules';
+import { Autoplay, Mousewheel } from 'swiper/modules';
 
 $(document).ready(function () {
 
-    const isMobile = () => window.innerWidth < 600;
+    const sharedConfig = {
+        modules: [Mousewheel],
+        slidesPerView: 1.15,
+        spaceBetween: 12,
+        loop: true,
+        mousewheel: { forceToAxis: true },
+        breakpoints: {
+            600:  { slidesPerView: 2, spaceBetween: 12 },
+            1024: { slidesPerView: 4, spaceBetween: 16 },
+        },
+    };
 
     if ($('.swiper-trending').length) {
         new Swiper('.swiper-trending', {
-            modules: [Navigation, Autoplay],
-            slidesPerView: 1.15,
-            spaceBetween: 12,
-            loop: true,
+            ...sharedConfig,
+            modules: [Autoplay, Mousewheel],
             autoplay: { delay: 7000, disableOnInteraction: false, pauseOnMouseEnter: true },
-            navigation: {
-                prevEl: '.swiper-trending .swiper-button-prev',
-                nextEl: '.swiper-trending .swiper-button-next',
-            },
-            breakpoints: {
-                600:  { slidesPerView: 2, spaceBetween: 12 },
-                1024: { slidesPerView: 4, spaceBetween: 16 },
-            },
         });
     }
 
     if ($('.swiper-similar').length) {
         new Swiper('.swiper-similar', {
-            modules: [Navigation, Autoplay],
-            slidesPerView: 1.15,
-            spaceBetween: 12,
-            loop: true,
+            ...sharedConfig,
+            modules: [Autoplay, Mousewheel],
             autoplay: { delay: 7000, disableOnInteraction: false, pauseOnMouseEnter: true },
-            navigation: {
-                prevEl: '.swiper-similar .swiper-button-prev',
-                nextEl: '.swiper-similar .swiper-button-next',
-            },
-            breakpoints: {
-                600:  { slidesPerView: 2, spaceBetween: 12 },
-                1024: { slidesPerView: 4, spaceBetween: 16 },
-            },
         });
     }
 
     if ($('.swiper-multiple').length) {
-        new Swiper('.swiper-multiple', {
-            modules: [Navigation],
-            slidesPerView: 1.15,
-            spaceBetween: 12,
-            loop: true,
-            navigation: {
-                prevEl: '.swiper-multiple .swiper-button-prev',
-                nextEl: '.swiper-multiple .swiper-button-next',
-            },
-            breakpoints: {
-                600:  { slidesPerView: 2, spaceBetween: 12 },
-                1024: { slidesPerView: 4, spaceBetween: 16 },
-            },
-        });
+        new Swiper('.swiper-multiple', sharedConfig);
     }
 });
