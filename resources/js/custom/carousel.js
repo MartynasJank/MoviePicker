e6cf207d38991f6033f0fd5069793a1109900abf
Martynas Jankauskas <martynasjank@gmail.com>
2026-05-22 13:55:11 +0300

    Remove mousewheel scroll control from all carousels

--- resources/js/custom/carousel.js
+++ resources/js/custom/carousel.js
@@ -1,19 +1,6 @@
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 
-function addWheelControl(swiper, el) {
-    let cooldown = false;
-    el.addEventListener('wheel', (e) => {
-        const delta = Math.abs(e.deltaX) >= 5 ? e.deltaX : e.deltaY;
-        if (Math.abs(delta) < 5) return;
-        e.preventDefault();
-        if (cooldown) return;
-        cooldown = true;
-        delta > 0 ? swiper.slideNext() : swiper.slidePrev();
-        setTimeout(() => { cooldown = false; }, 600);
-    }, { passive: false });
-}
-
 $(document).ready(function () {
 
     const sharedConfig = {
@@ -30,8 +17,7 @@ $(document).ready(function () {
     };
 
     if ($('.swiper-trending-day').length) {
-        const s = new Swiper('.swiper-trending-day', sharedConfig);
-        addWheelControl(s, document.querySelector('.swiper-trending-day'));
+        new Swiper('.swiper-trending-day', sharedConfig);
         let weekSwiper = null;
 
         $('#trend-day, #trend-week').on('click', function () {
@@ -44,18 +30,15 @@ $(document).ready(function () {
 
             if (isWeek && !weekSwiper) {
                 weekSwiper = new Swiper('.swiper-trending-week', sharedConfig);
-                addWheelControl(weekSwiper, document.querySelector('.swiper-trending-week'));
             }
         });
     }
 
     if ($('.swiper-similar').length) {
-        const s = new Swiper('.swiper-similar', sharedConfig);
-        addWheelControl(s, document.querySelector('.swiper-similar'));
+        new Swiper('.swiper-similar', sharedConfig);
     }
 
     if ($('.swiper-multiple').length) {
-        const s = new Swiper('.swiper-multiple', sharedConfig);
-        addWheelControl(s, document.querySelector('.swiper-multiple'));
+        new Swiper('.swiper-multiple', sharedConfig);
     }
 });
