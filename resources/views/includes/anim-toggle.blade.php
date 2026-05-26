4922e086e80df00becd366fd008de5eb4694cfd8
Martynas Jankauskas <martynasjank@gmail.com>
2026-05-26 16:56:43 +0300

    Add shared animation toggle partial

---
+++ resources/views/includes/anim-toggle.blade.php
@@ -0,0 +1,8 @@
+<label class="flex items-center gap-2 text-xs text-gray-400 cursor-pointer select-none">
+    <span class="relative inline-block w-9 h-5">
+        <input type="checkbox" data-anim-toggle class="sr-only">
+        <span data-anim-track class="block w-9 h-5 rounded-full transition-colors duration-200"></span>
+        <span data-anim-thumb class="absolute top-0.5 left-0.5 w-4 h-4 rounded-full bg-white transition-transform duration-200 shadow"></span>
+    </span>
+    Animation
+</label>
