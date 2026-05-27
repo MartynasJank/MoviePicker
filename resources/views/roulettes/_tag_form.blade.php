0c13732b53c40b3df08ea989f6e33e35a5e0be89
Martynas Jankauskas <martynasjank@gmail.com>
2026-05-27 11:34:11 +0300

    Fix Platform/Language/Country grid: 3 columns not 4

--- resources/views/roulettes/_tag_form.blade.php
+++ resources/views/roulettes/_tag_form.blade.php
@@ -118,7 +118,7 @@
         </div>
     </div>
 
-    <div class="grid grid-cols-2 sm:grid-cols-4 gap-4">
+    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
 
         {{-- Platform --}}
         <div>
