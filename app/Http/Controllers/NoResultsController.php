ac011a3f4ad23a72f954af290a887e2ea3a5e5d2
Martynas Jankauskas <martynasjank@gmail.com>
2026-05-28 14:47:05 +0300

    No-results page: prefilled criteria modal instead of blank criteria page, remove Roll Again, criteria page always blank including sub-genres

--- app/Http/Controllers/NoResultsController.php
+++ app/Http/Controllers/NoResultsController.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TmdbClient;
+use App\Services\MovieService;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class NoResultsController extends Controller
 {
-    public function __invoke(Request $request): View
+    public function __invoke(Request $request, TmdbClient $tmdb, MovieService $movieService): View
     {
-        $type = $request->query('type', 'movie');
-        $isTv = $type === 'tv';
+        $type       = $request->query('type', 'movie');
+        $isTv       = $type === 'tv';
+        $user_input = session($isTv ? 'tvInput' : 'userInput', []);
 
         return view('no-results', [
-            'rollAgainUrl' => $isTv ? url('/tv/pick') : url('/movie'),
-            'criteriaUrl'  => $isTv ? url('/tv/criteria') : url('/criteria'),
-            'randomUrl'    => $isTv ? url('/tv/pick?i=new') : url('/movie?i=new'),
+            'isTv'           => $isTv,
+            'randomUrl'      => $isTv ? url('/tv/pick?i=new') : url('/movie?i=new'),
+            'user_input'     => $user_input,
+            'all_genres'     => $movieService->genres($tmdb, $isTv ? 'tv' : 'movie'),
+            'providersArray' => $movieService->buildProvidersArray($tmdb),
         ]);
     }
 }
