ac82c5ee2d99053cefb4b80e72fc955123977b24
Martynas Jankauskas <martynasjank@gmail.com>
2026-06-01 17:26:17 +0300

    Clean up routes, add InternalOnly middleware to proxy endpoints, remove dead routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'admin'    => \App\Http\Middleware\AdminMiddleware::class,
+        'internal' => \App\Http\Middleware\InternalOnly::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
