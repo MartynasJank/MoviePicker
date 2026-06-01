dd93bddabe0cb6cd496b3cd7982faec900bcd308
Martynas Jankauskas <martynasjank@gmail.com>
2026-06-01 13:42:21 +0300

    Restore fetchpriority=high on LCP image, lazy load remaining bg posters

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
                          alt=""
                          width="185" height="278"
                          class="w-full h-full object-cover {{ $loop->index >= 6 ? 'hidden md:block' : '' }}"
-                         loading="lazy">
+                         @if($loop->first) fetchpriority="high" @else loading="lazy" @endif>
                 @endforeach
             </div>
         @endif
