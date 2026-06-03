c3eabe084a0f06172653c39b3b4ea338f19f32d9
Martynas Jankauskas <martynasjank@gmail.com>
2026-06-03 14:24:13 +0300

    Add AMAZON_AFFILIATE_TAG config

--- config/api.php
+++ config/api.php
@@ -5,5 +5,6 @@ return [
     'OMDB'          => env('OMDB_API_KEY', 'No Key'),
     'YOUTUBE'       => env('YOUTUBE_API_KEY', 'No Key'),
     'contact_email' => env('CONTACT_EMAIL', 'info@moviepickr.com'),
-    'admin_email'   => env('ADMIN_EMAIL', ''),
+    'admin_email'          => env('ADMIN_EMAIL', ''),
+    'amazon_affiliate_tag' => env('AMAZON_AFFILIATE_TAG', ''),
 ];
